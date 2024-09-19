331b316ab82d2e0e1cb4b53673c99ba0c075de23
cocktailpeanut <cocktailpeanuts@proton.me>
2024-09-19 08:13:28 -0400

    img2vid

--- install.js
+++ install.js
@@ -5,7 +5,25 @@ module.exports = {
       method: "shell.run",
       params: {
         message: [
-          "git clone https://github.com/peanutcocktail/CogVideo app",
+          "git clone https://github.com/THUDM/CogVideo app",
+        ]
+      }
+    },
+    {
+      method: "fs.copy",
+      params: {
+        src: "tabui.py",
+        dest: "app/inference/gradio_composite_demo/tabui.py"
+      }
+    },
+    // Edit this step with your custom install commands
+    {
+      method: "shell.run",
+      params: {
+        venv: "../../env",                // Edit this to customize the venv folder path
+        path: "app/inference/gradio_composite_demo",                // Edit this to customize the path to start the shell from
+        message: [
+          "pip install -r requirements.txt",
         ]
       }
     },
@@ -19,17 +37,6 @@ module.exports = {
           path: "app",                // Edit this to customize the path to start the shell from
           // xformers: true   // uncomment this line if your project requires xformers
         }
-      }
-    },
-    // Edit this step with your custom install commands
-    {
-      method: "shell.run",
-      params: {
-        venv: "env",                // Edit this to customize the venv folder path
-        path: "app",                // Edit this to customize the path to start the shell from
-        message: [
-          "pip install -r requirements.txt",
-        ]
       }
     },
     {
--- start.js
+++ start.js
@@ -4,13 +4,13 @@ module.exports = {
     {
       method: "shell.run",
       params: {
-        venv: "../env",                // Edit this to customize the venv folder path
+        venv: "../../env",                // Edit this to customize the venv folder path
         env: {
           "PYTORCH_ENABLE_MPS_FALLBACK": "1"  
         },                   // Edit this to customize environment variables (see documentation)
-        path: "app/inference",                // Edit this to customize the path to start the shell from
+        path: "app/inference/gradio_composite_demo",                // Edit this to customize the path to start the shell from
         message: [
-          "python gradio_web_demo.py"
+          "python tabui.py"
         ],
         on: [{
           // The regular expression pattern to monitor.
